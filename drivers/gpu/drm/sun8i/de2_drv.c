e2950b1fcf152903c37ef746fffd705c650df83b
Ondrej Jirman <megous@megous.com>
2017-05-18 18:35:22 +0200

    DE2 support for 4.12

--- drivers/gpu/drm/sun8i/de2_drv.c
+++ drivers/gpu/drm/sun8i/de2_drv.c
@@ -75,7 +75,6 @@ static struct drm_driver de2_drm_driver = {
 	.driver_features	= DRIVER_GEM | DRIVER_MODESET | DRIVER_PRIME |
 					DRIVER_ATOMIC,
 	.lastclose		= de2_lastclose,
-	.get_vblank_counter	= drm_vblank_no_hw_counter,
 	.enable_vblank		= de2_enable_vblank,
 	.disable_vblank		= de2_disable_vblank,
 	.gem_free_object	= drm_gem_cma_free_object,
